ee1680d65e3fd5f87d3e38eac542dfab53f20cd1
Piotr <alx@kurs.pl>
2018-06-09 11:29:33 +0200

    WERSJA FINALNA TEST ROSNACY I MALEJACY 11:29 Piotr

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -17,23 +17,8 @@ public class SortingAlgorithmTest {
         System.out.println("Metoda zostanie wykonana po kazdym tescie");
     }
 
-    @Test
-    public void testSorting() throws Exception {
-
-        double[] toSort = {3, 12, 2, 5, 14, 43.56};
-        double[] posortowana = {2, 3, 5, 12, 14, 43.56};
-        boolean asc = true;
-
-        algorytm.sort(toSort, asc);
-
-        for (int i = 0; i < toSort.length; i++) {
-
-            Assert.assertEquals("Test nieudany", posortowana[i], toSort[i], 0.01);
-
-        }
-    }
         @Test
-        public void testSortowania() throws Exception{
+        public void testSortowaniaRosnacy() throws Exception{
             boolean asc = true;
             double[] toSort = {3,5,3,4,5,7};
             double[] posortowana = {3,3,4,5,5,7};
@@ -48,7 +33,7 @@ public class SortingAlgorithmTest {
         }
 
         @Test
-        public void testSortowania2() throws Exception{
+        public void testSortowaniaMalejacy() throws Exception{
             boolean asc = false;
             double[] toSort = {3,5,3,4,5,7};
             double[] posortowana = {7,5,5,4,3,3};
@@ -59,24 +44,6 @@ public class SortingAlgorithmTest {
                 Assert.assertEquals("Błąd sortowania na pozycji " + i,posortowana[i],toSort[i],0.01);
             }
             System.out.println("Test2 zaliczony");
-    }
-    //Radek1
-    //Radek
-    @Test
-    public void testSortowania4() throws Exception {
-        double[] toSort = {3, 7, 4, 2, 6};
-        double[] posortowana = {2, 3, 4, 6, 7};
-
-        algorytm.sort(toSort, true);
-
-        for (int i = 0; i < toSort.length; i++) {
-
-
-            Assert.assertEquals("Blad przy sortowaniu na pozycji: " + i,
-                    posortowana[i], toSort[i], 0.01);
-
-        }
-
     }
 }
 
