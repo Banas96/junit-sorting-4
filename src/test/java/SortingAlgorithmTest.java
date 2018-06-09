c2e729640507755a3a2fc42e50c6176f95d26fe8
Marcin <marcinbanas@wp.pl>
2018-06-09 10:38:46 +0200

    test Marcina

--- src/test/java/SortingAlgorithmTest.java
+++ src/test/java/SortingAlgorithmTest.java
@@ -5,21 +5,17 @@ import org.junit.Test;
 
 public class SortingAlgorithmTest {
 
-
     private SortingAlgorithms algorytm;
 
     @Before
     public void setUp() throws Exception {
         algorytm = new SortingAlgorithms();
-
     }
-
 
     @After
     public void tearDown() {
         System.out.println("Metoda zostanie wykonana po kazdym tescie");
     }
-
 
     @Test
     public void testSorting() throws Exception {
@@ -30,26 +26,39 @@ public class SortingAlgorithmTest {
 
         algorytm.sort(toSort, asc);
 
-        for(int i = 0; i < toSort.length; i++) {
+        for (int i = 0; i < toSort.length; i++) {
 
             Assert.assertEquals("Test nieudany", posortowana[i], toSort[i], 0.01);
 
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
     }
+        @Test
+        public void testSortowania() throws Exception{
+            boolean asc = true;
+            double[] toSort = {3,5,3,4,5,7};
+            double[] posortowana = {3,3,4,5,5,7};
 
+            int i = 0;
+            algorytm.sort(toSort,asc);
+            while(i< toSort.length){
+                Assert.assertEquals("Błąd sortowania na pozycji " + i,posortowana[i],toSort[i],0.01);
+                i++;
+            }
+            System.out.println("Test zaliczony");
+        }
 
+        @Test
+        public void testSortowania2() throws Exception{
+            boolean asc = false;
+            double[] toSort = {3,5,3,4,5,7};
+            double[] posortowana = {7,5,5,4,3,3};
 
+            int i = 0;
+            algorytm.sort(toSort,asc);
+            while(i< toSort.length){
+                Assert.assertEquals("Błąd sortowania na pozycji " + i,posortowana[i],toSort[i],0.01);
+            }
+            System.out.println("Test2 zaliczony");
+    }
 }
+
